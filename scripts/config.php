bf3481ccba83367101d3b05a513aec6ac1e4bd8f
Juan Manuel Rodríguez <jmrodriguez@ximdex.com>
2014-10-28 11:51:46 +0100

    Import project and first fixes related with config file

---
+++ scripts/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Configuración parámetros de Congreso transparente
+
+$SERVER_URL_XIMDEX_INSTANCE ="http://lab13.ximdex.net/ximdexct";
+$INDEX_ID = 10177;
+$RNG_NODE_ID = 10175;
+$DOCUMENT_FOLDER_ID = 10148;
+$XIMDEX_USER = "ximdex";
+$XIMDEX_PASS = "ximdexct";
